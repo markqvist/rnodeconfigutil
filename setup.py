cf44a76cc09dd746efbe47a598907785a920d371
Mark Qvist <mark@adepta.io>
2020-06-03 10:16:00 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rnodeconf",
-    version="0.9.10",
+    version="1.0.0",
     author="Mark Qvist",
     author_email="mark@unsigned.io",
     description="Configuration Utility for RNode hardware",
